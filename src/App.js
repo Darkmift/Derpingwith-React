7b8e41abbab264279cb47f05a75dc0253fa0d8f3
Darkmift <Darkmift@users.noreply.github.com>
2019-06-07 19:10:19 +0300

    commit #2 props,propTypes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from "react";
+import { Navbar } from "./components/layout/Navbar";
 import "./CSS/App.css";
 
 class App extends Component {
-  bar = () => "bar";
-
   render() {
-    const name = "Bob";
-    const foo = () => "inner";
-
-    const loading = false;
-    const showname = true;
-
-    if (loading) return <h4>Loading...</h4>;
-
     return (
       <div className="App">
-        {loading ? (
-          <h1>Loading...</h1>
-        ) : (
-          <h1>Hello {showname ? name : "Guest"}!</h1>
-        )}
-        <br /> this is an inner: {foo()}.
-        <br /> this is a class method: {this.bar()}
+        {/* <Navbar title="Github-finder" icon="fab fa-github" /> */}
+        {/* <Navbar title={[1,2,3,4]} />
+         *  when title=PropTypes.string.isRequired,this throws consolee type error
+         */}
+        <Navbar />
       </div>
     );
   }
