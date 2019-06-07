d9c39beb46c6b8ac2210ac2eadd9974f3e6ea5bd
Darkmift <Darkmift@users.noreply.github.com>
2019-06-07 18:36:24 +0300

    1st commit-variables,Class,conditionals

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./CSS/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  bar = () => "bar";
+
+  render() {
+    const name = "Bob";
+    const foo = () => "inner";
+
+    const loading = false;
+    const showname = true;
+
+    if (loading) return <h4>Loading...</h4>;
+
+    return (
+      <div className="App">
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <h1>Hello {showname ? name : "Guest"}!</h1>
+        )}
+        <br /> this is an inner: {foo()}.
+        <br /> this is a class method: {this.bar()}
+      </div>
+    );
+  }
 }
 
 export default App;
