75043b4c8bbafc90ed3095a776ec479279be9c04
Darkmift <Darkmift@users.noreply.github.com>
2019-06-09 19:42:48 +0300

    added router,routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
+import Search from "./components/users/Search";
 import Users from "./components/users/Users";
+import About from "./components/pages/About";
 import axios from "axios";
 import Link from "./url";
-import Search from "./components/users/Search";
+
 import "./CSS/App.css";
 
 class App extends Component {
   state = { users: [], loading: false, alert: null };
 
-  async componentDidMount() {
-    const Url = new Link();
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `${Url.link}${Url.users}?${Url.credentials}${Url.amount_defined_string}`
-    );
-    this.setState({ users: res.data, loading: false });
-  }
+  // async componentDidMount() {
+  //   const Url = new Link();
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `${Url.link}${Url.users}?${Url.credentials}${Url.amount_defined_string}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  // }
 
   searchUsers = async text => {
     const Url = new Link();
@@ -40,19 +43,35 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={this.state.users.length ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About}></Route>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
